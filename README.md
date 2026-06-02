8b2914667251356add55d8d9893c4c5ca4978d98
Lucas Zarate <maxolucas@gmail.com>
2026-06-01 23:35:43 -0300

    Agregacion de views

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 ## App del Trabajo Integrador final
 
+# Guia de uso
+
+### Levantar el Proyecto Fotaza!
+
+- Instalar todas las dependencias con ' npm install '
+- Iniciar ' npm run dev ' para entrar en el modo v
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 const PORT = process.env.PORT;
 
 //middlewares
+app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -14,6 +15,17 @@ app.set('view engine', 'pug');
 app.set('views', './views');
 
 //rutas
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.get('/register', (req, res) => {
+    res.render('register');
+});
 
 //Servidor
 app.listen(PORT, () => {
